b6d505a61fb437698c05a844f2457f1c0e6388e8
ptesolin <ptesolin@ltu.edu>
2017-11-27 18:46:56 -0500

    Commit

--- project.c
+++ project.c
@@ -1,3 +1,31 @@
+/*********************************************************************************************************************************
+**
+** Module Name:   project.c
+**
+** Module Description: Main Project C file 
+
+**This program performs two main functions. The first is to calculate the first order transfer function of a motor based on a step
+**input. When the Calibrate function is called and executed a step input sent to the motor with via a PWM signal with 100 percent 
+**duty cycle. The voltage across the motor is reduced by a voltage divider then read into an ADC channel. The motor speed is read 
+**every 10mS and stored in memory until the speed remains constant. Once the speed remains constant the time constant (t) for the 
+**transfer function is calculated by finding the time the motor took to reach its steady state speed multiplied by (1-e^-1). The 
+**gain (K) value is calculated by dividing its final speed by the input voltage. Once these values are known a first order transfer
+**function can be derived in the form k/(ts+1).
+**The second function asks the user for a desired speed then through PWM the microcontroller maintains that speed regardless of the 
+**load placed on the motor. Every 10mS the encoder reads the motors current speed. The difference between the desired speed and the 
+**actual speed is multiplied by a gain and adjusts the PWM signal to compensate for this load. In addition to compensating for the 
+**error between current speed and desired speed the controller also adds compensation for small errors over long periods and 
+**compensation for rate of change. 
+
+**
+**********************************************************************************************************************************
+**
+** Author(s):		Kodey Boreo, Phillip Tesolin 
+**
+**********************************************************************************************************************************/
+
+
+
 #include "project.h"
 #include <stdio.h>
 #include <stdint.h>
